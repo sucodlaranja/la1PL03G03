7bc28a3de575b904537bddc049be6913eb701820
João Martins <55655155+sucodlaranja@users.noreply.github.com>
2020-03-10 15:44:27 +0000

    Delete logica_programa.h

--- projeto/Jogo/logica_programa.h
+++
@@ -1,8 +0,0 @@
-#include "camada_dados.h"
-
-#ifndef PROJETO_LOGICA_POGRAMA_H
-void mostrar_tabuleiro(ESTADO estado);
-int jogar(ESTADO *e, COORDENADA c);
-#define PROJETO_LOGICA_POGRAMA_H
-
-#endif //PROJETO_LOGICA_POGRAMA_H
