79b637253ad4f4a1e13949cf1ab15636d23e749c
Miguel-Neiva <61981804+Miguel-Neiva@users.noreply.github.com>
2020-04-22 18:33:54 +0100

    Add files via upload

--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,12 +1,18 @@
 var indexSectionsWithContent =
 {
+  0: "cejn",
+  1: "cejn"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes"
 };
 
